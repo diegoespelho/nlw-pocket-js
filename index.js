384386feab3b9a4b9675a597e6888b8629270235
Diego Espelho <diego.espelho@gmail.com>
2024-09-10 20:59:42 -0300

    create cadastrar meta

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const { select } = require("@inquirer/prompts");
+const { select, input } = require("@inquirer/prompts");
+
+let meta = {
+  value: "Tomar 3L de água por dia",
+  checked: false,
+};
+
+let metas = [meta];
+
+const cadastrarMeta = async () => {
+  const meta = await input({ message: "Digite a meta:" });
+
+  if (meta.length == 0) {
+    console.log("A meta não pode ser vazia.");
+    return;
+  }
+
+  metas.push({ value: meta, checked: false });
+};
 
 const start = async () => {
   while (true) {
@@ -11,7 +29,7 @@ const start = async () => {
         },
         {
           name: "Listar metas",
-          value: "listar"
+          value: "listar",
         },
         {
           name: "Sair",
@@ -22,16 +40,17 @@ const start = async () => {
 
     switch (opcao) {
       case "cadastrar":
-        console.log("vamos cadastrar");
+        await cadastrarMeta();
+        console.log(metas);
         break;
       case "listar":
         console.log("vamos listar");
         break;
       case "sair":
-        console.log("até a próxima")
+        console.log("até a próxima");
         return;
     }
   }
 };
 
-start()
+start();
