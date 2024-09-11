adac33f132b5a35b571b7dc632574583770195dc
Diego Espelho <diego.espelho@gmail.com>
2024-09-11 17:28:06 -0300

    goals realised

--- index.js
+++ index.js
@@ -46,6 +46,22 @@ const listarMetas = async () => {
   console.log("Meta(s) marcada(s) como concluída(s)");
 };
 
+const metasRealizadas = async () => {
+  const realizadas = metas.filter((meta) => {
+    return meta.checked;
+  });
+
+  if (realizadas.length == 0) {
+    console.log("Não existem metas realizadas! :(");
+    return;
+  }
+
+  await select({
+    message: "Metas realizadas",
+    choices: [...realizadas],
+  });
+};
+
 const start = async () => {
   while (true) {
     const opcao = await select({
@@ -58,6 +74,10 @@ const start = async () => {
         {
           name: "Listar metas",
           value: "listar",
+        },
+        {
+          name: "Metas realizadas",
+          value: "realizadas",
         },
         {
           name: "Sair",
@@ -73,6 +93,9 @@ const start = async () => {
         break;
       case "listar":
         await listarMetas();
+        break;
+      case "realizadas":
+        await metasRealizadas();
         break;
       case "sair":
         console.log("até a próxima");
